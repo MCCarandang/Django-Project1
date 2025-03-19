aee2e7dbd25b6a74f91c8dfde376bd3f4f0397bf
MCCarandang <carandangmariane19@gmail.com>
2025-03-19 09:48:39 +0800

    Delete Lesson10/myproject directory

--- Lesson10/myproject/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'posts'      #This says that the urlpatterns below are inside the posts app
-
-urlpatterns = [
-    path('', views.posts_list, name = "list"),
-    path('<slug:slug>', views.post_page, name = "page"),
-]
